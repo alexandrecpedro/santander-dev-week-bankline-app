372bf2db924e292f8c018001b6558095d741d262
Alexandre Pedro <80993336+alexandrecpedro@users.noreply.github.com>
2022-05-09 21:26:39 -0300

    Bank statement generation system from Santander Bank

--- bankline-api/src/main/java/com/dio/santander/bankline/api/repository/AccountHolderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dio.santander.bankline.api.repository;
-
-import com.dio.santander.bankline.api.model.AccountHolder;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountHolderRepository extends JpaRepository<AccountHolder, Integer> {
-}
